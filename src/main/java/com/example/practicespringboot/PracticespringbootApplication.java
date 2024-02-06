13b10fc9941f550dee093cbbd4adf838ff777e58
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2024-02-07 00:28:07 +0900

    test

---
+++ src/main/java/com/example/practicespringboot/PracticespringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.practicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticespringbootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PracticespringbootApplication.class, args);
+	}
+
+}
